f3ad02288df133a83b639b0385bb675d352249e8
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-04-10 23:45:07 +0300

    make  register of user in project

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Authenticatable
 {
+    use LaratrustUserTrait;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -17,10 +19,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $primaryKey="id";
+   
     protected $fillable = [
         'name',
-        'full_name',
-        'avater',
         'email',
         'password',
     ];
