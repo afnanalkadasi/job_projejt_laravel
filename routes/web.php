5b86edf52a638b2548114812f841de29795a1019
afnanalkadasi <afnanalkadasi22@gmail.com>
2022-04-11 05:08:33 +0300

    make crud of category in admin in project

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\user\ExperienceController;
 use App\Http\Controllers\user\CourseController;
 use App\Http\Controllers\user\EducationController;
 
-
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\AuthController;
 // use App\Http\Controllers\admin\CategoryController;
@@ -15,16 +14,8 @@ use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\SettingsController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
 // Route::group(['prefix' => LaravelLocalization::setLocale(),
 //     'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function(){ //...
  
@@ -35,21 +26,31 @@ Route::view('/about_as','front.about_as')->name('about_as');
 Route::view('/contact','front.contact')->name('contact');
 Route::view('/company','front.company')->name('company');
 Route::view('/details_job','front.details_job')->name('details_job');
-Route::view('/information1','front.information1')->name('information1');
 Route::view('/job','front.jobs')->name('job');
-Route::view('/other_users','front.other_users')->name('other_users');
 Route::view('/user-prof','front.user-prof')->name('user-prof');
 Route::view('/service','front.service')->name('service');
-Route::view('/login','auth.login')->name('login');
-Route::get('/sign_up',[AuthController::class,'create'])->name('sign_up');
-Route::post('/save_user',[AuthController::class,'register'])->name('save_user');
-Route::get('/logout',[AuthController::class,'logout'])->name('logout');
-Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-Route::post('/do_login',[AuthController::class,'login'])->name('do_login');
+
+
+
 
         ///////////////////////// Start admins of admin Dashboard /////////////////////////
+        Route::get('/generate_roles',[SettingsController::class,'generateRoles'])->name('generate_roles');
 
-      
+        Route::get('/admin/users',[AuthController::class,'listAll'])->name('users');
+        Route::view('/edit_user','admin.edit_user')->name('edit_user');
+        Route::post('/save_user',[AuthController::class,'register'])->name('save_user');
+        Route::get('/sign_up',[AuthController::class,'create'])->name('sign_up');
+        Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+        Route::post('/do_login',[AuthController::class,'login'])->name('do_login');
+        Route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
+        Route::get('/admin/categories',[CategoryController::class,'listAll'])->name('categories');
+        Route::get('/admin/add_category',[CategoryController::class,'create'])->name('add_category');
+        Route::post('/admin/save_category',[CategoryController::class,'store'])->name('save_category');
+        Route::get('/admin/edit_category/{cat_id}',[CategoryController::class,'edit'])->name('edit_category');
+        Route::get('/admin/toggle_category/{cat_id}',[CategoryController::class,'toggle'])->name('toggle_category');
+        Route::post('/admin/update_category/{cat_id}',[CategoryController::class,'update'])->name('update_category');
+
         Route::view('/services','admin.services')->name('services');
         Route::view('/add_service','admin.add_service')->name('add_service');
         Route::view('/edit_service','admin.edit_service')->name('edit_service');
@@ -59,18 +60,10 @@ Route::post('/do_login',[AuthController::class,'login'])->name('do_login');
         Route::view('/jobs','admin.jobs')->name('jobs');
         Route::view('/add_job','admin.add_job')->name('add_job');
         Route::view('/edit_job','admin.edit_job')->name('edit_job');
-        Route::view('/users','admin.users')->name('users');
-        Route::view('/add_user','admin.add_user')->name('add_user');
-        Route::view('/edit_user','admin.edit_user')->name('edit_user');
+      
+       
 
 
-        Route::get('/admin/categories',[CategoryController::class,'listAll'])->name('categories');
-        Route::get('/admin/add_category',[CategoryController::class,'create'])->name('add_category');
-        Route::post('/admin/save_category',[CategoryController::class,'store'])->name('save_category');
-        Route::view('/admin/edit_category','admin.edit_category')->name('edit_category');
-
-        Route::get('/generate_roles',[SettingsController::class,'generateRoles'])->name('generate_roles');
-     
         
         ///////////////////////// Start   user Dashboard /////////////////////////
           Route::view('/information','user.information')->name('information');
